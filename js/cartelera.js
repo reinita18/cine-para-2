d8f82b649150c8a37dda3e0828498b6d508cfaf9
reinita18 <113558054+reinita18@users.noreply.github.com>
2026-07-25 22:04:33 -0700

    Update cartelera.js

--- js/cartelera.js
+++ js/cartelera.js
@@ -56,6 +56,7 @@ export function renderBienvenida(contenedor, contexto) {
       </div>
     </section>
   `;
+  
 
   contenedor.querySelector('[data-action="movies"]').addEventListener("click", () => {
     contexto.navegar("movies");
